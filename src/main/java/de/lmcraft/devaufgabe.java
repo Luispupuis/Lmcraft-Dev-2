59a5302030a9c198a0b0c5a923548eed4cadf8ef
Luispupuis <119443877+Luispupuis@users.noreply.github.com>
2024-06-11 08:26:44 +0200

    Update devaufgabe.java

--- src/main/java/de/lmcraft/devaufgabe.java
+++ src/main/java/de/lmcraft/devaufgabe.java
@@ -1,1 +1,17 @@
+package de.lmcraft.devaufgabe;
 
+import de.lmcraft.devaufgabe.commands.clearchat;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevAufgabe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
